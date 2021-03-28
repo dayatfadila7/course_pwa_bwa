b7d1d1b887aa7f975f42cc74f5f396b448e8eaf2
dayatfadila <dayat.recovery@gmail.com>
2021-03-28 16:33:35 +0700

    video 3 and video 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import Header from "./components/Header";
+import React from "react";
+
+import Header from "./components/Header.js";
+import Hero from "./components/Hero.js";
+import Browse from "./components/Browse.js";
+import Arrived from "./components/Arrived.js";
+import Clients from "./components/Clients.js";
+import Modal from "./components/Modal.js";
+import Footer from "./components/Footer.js";
 
 function App() {
+    const [showModal, setShowModal] = React.useState(false);
+
+    function handleShowModal(event) {
+        setShowModal(!showModal);
+    }
     return (
-        <Header />
+        <>
+            <Header />
+            <Hero handleShowModal={handleShowModal} />
+            <Browse />
+            <Arrived />
+            <Clients />
+            <Footer />
+            {showModal && <Modal handleShowModal={handleShowModal} />}
+        </>
     );
 }
 
